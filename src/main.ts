32a7d8b28cbc0586bd8b0e80b59cb9865ae3ffd7
graceDev1 <grace.devolop@gmail.com>
2021-02-09 11:46:35 +0200

    last commit

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
-import express, { Application} from 'express';
+import express, { Application, Request, Response} from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import schema from './schema';
 
 const app: Application = express()
 
+app.get('/', (req: Request,res: Response)=>{
+    res.send('<h1>Hi, please go to /graphql</h1>');
+})
+
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
-}))
+}));
 
 const port = process.env.PORT || 4000;
 
